432c97aada7fdbc67c5f8afe5f7a8f9c30680f1c
Jon Reyes <jonath.lt3@gmail.com>
2023-10-06 16:43:53 -0700

    Se agrego el boton limpiar y el guardado de registros

--- js/app.js
+++ js/app.js
@@ -1,20 +1,114 @@
-//Declarar Variables
+/* DECLARAR VARIABLES */
 
-const btnCalcular = document.getElementById('btnCalcular')
+const btnCalcular = document.getElementById('btnCalcular');
+const btnLimpiar = document.getElementById('btnLimpiar');
 
-btnCalcular.addEventListener('click', function(){
-    // Obtener los datos de los input
-    let valorAuto = document.getElementById('valorAuto').value;
-    let pInicial = document.getElementById('porcentaje').value;
-    let plazos  = document.getElementById('plazos').value;
+// se declaran los valores
+let aside = document.getElementById('aside');
+let estado = document.getElementById('status');
 
-    // Hacer los calculos
-    let pagoInicio = valorAuto * (pInicial/100);
-    let totalFin = valorAuto - pagoInicio;
-    let pagoMensual = totalFin / plazos;
+// se inician las variables
+let valorAuto_e = document.getElementById('valorAuto');
+let pInicial_e = document.getElementById('porcentaje');
+let plazos_e = document.getElementById('plazos');
+let pagoInicial_e = document.getElementById('pagoInicial'); 
+let totalFin_e = document.getElementById('totalFin');
+let pagoMensual_e = document.getElementById('pagoMensual');
 
-    // Mostrar 
-    document.getElementById('pagoInicial').value = pagoInicio;
-    document.getElementById('totalFin').value = totalFin;
-    document.getElementById('pagoMensual').value = pagoMensual;
-})
+btnCalcular.addEventListener('click', function() {
+  // se calculan los valores una vez se presione calcular
+  let valorAuto = valorAuto_e.value;
+  let pInicial = pInicial_e.value;
+  let plazos = plazos_e.value;
+  
+  // hacer los cálculos
+  let pagoInicial = valorAuto * (pInicial / 100);
+  let totalFin = valorAuto-pagoInicial;
+  let pagoMensual = totalFin / plazos;
+
+  // mostrar los datos
+  pagoInicial_e.value = pagoInicial;
+  totalFin_e.value = totalFin;
+  pagoMensual_e.value = pagoMensual;
+
+  if(
+    valorAuto >= 0 && 
+    pInicial >= 0 && 
+    valorAuto.length > 0 &&
+    pInicial.length > 0
+    )
+  {
+    let newParagraph = document.createElement('table');
+    newParagraph.innerHTML =
+    `
+    <thead>
+      <th>Dato</th>
+      <th>Valor</th>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Valor de Auto</td> 
+        <td>${valorAuto}</td>
+      </tr>
+      <tr>
+        <td>Porcentaje de Pago Inicial</td>
+        <td>${pInicial}</td>
+      </tr>
+      <tr>
+        <td>Plazos</td>
+        <td>${plazos} meses</td>
+      </tr>
+      <tr><td style="background-color: gainsboro; padding: 10px;" colspan="2">Cálculos</td></tr>
+      <tr>
+        <td>Pago Inicial</td>
+        <td>${pagoInicial}</td>
+      </tr>
+      <tr>
+        <td>Total a Financiar</td>
+        <td>${totalFin}</td>
+      </tr>
+      <tr>
+        <td>Pago Mensual</td>
+        <td>${pagoMensual}</td>
+      </tr>
+    </tbody>
+    `;
+    aside.appendChild(newParagraph);
+    // we update the status
+    estado.style.backgroundColor = 'green';
+    estado.style.color = 'white';
+    estado.innerHTML = 'SE GUARDO EL REGISTRO CORRECTAMENTE';
+  }
+  else {
+    estado.style.backgroundColor = 'red';
+    estado.style.color = 'white';
+    estado.innerHTML = 'INGRESE VALORES QUE SEAN VALIDOS';
+  }
+});
+
+//LIMPIAR VALORES
+btnLimpiar.addEventListener('click', () => {
+  valorAuto_e.value = '';
+  pInicial_e.value = '';
+  plazos_e.selectedIndex = 0;
+  pagoInicial_e.value = '';
+  totalFin_e.value = '';
+  pagoMensual_e.value = '';
+
+  // se limpia el status
+  estado.style.color = "black";
+  estado.style.backgroundColor = "white";
+  estado.innerHTML = 'SE LIMPIARON LOS VALORES'
+
+});
+
+//Funcion para mostrar el promedio de los elementos de array
+
+
+//Funcion para mostrar los valores pares de un arreglo
+
+
+//Funcion para mostrar el valor mayor de los elementos de un arreglo
+
+
+//Funcion para llenar con valores aleatoriosel arreglo
